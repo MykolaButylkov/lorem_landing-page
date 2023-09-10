5f3a74ddec2e0843e837274168f3753c54f795b9
Mykola Butylkov <80322190+MykolaButylkov@users.noreply.github.com>
2023-09-10 16:39:24 +0300

    5th commit

--- script/script.js
+++ script/script.js
@@ -4,8 +4,6 @@ const footerSlider = document.querySelector('.footer__footer-list-slider');
 const body = document.querySelector('body');
 
 gsap.registerPlugin(ScrollTrigger);
-console.log(footerSlider.clientHeight)
-console.log(window.innerHeight)
   gsap.to(footerSlider, {
     x: () => "-=" +
       (footerSlider.clientWidth - window.innerWidth),
@@ -18,3 +16,19 @@ console.log(window.innerHeight)
     }
   });
 
+
+const footerListDescription = document.querySelectorAll('.footer-list-slider__descr')
+
+if (window.innerWidth <= 570) {
+  footerListDescription.forEach((el, i) => {
+    if (i === 0) {
+      el.textContent = 'Lorem ipsum dolor sit amet consectetur. Eget volutpat et sit nunc metus auctor morbi. Lectus auctor ac senectus molestie ut tincidunt sit convallis amet. Nullam quis. ';
+    } else if (i === 1) {
+      el.textContent = 'Lorem ipsum dolor sit amet consectetur. Eget volutpat et sit nunc metus auctor morbi. Lectus auctor ac senectus molestie ut tincidunt sit convallis amet.';
+    } else if (i === 2) {
+      el.textContent = 'Lorem ipsum dolor sit amet consectetur. Eget volutpat et sit nunc metus auctor morbi. Lectus auctor ac senectus molestie.';
+    } else if (i === 3) {
+      el.textContent = 'Lorem ipsum dolor sit amet consectetur. Eget volutpat et sit nunc metus auctor morbi. Lectus auctor ac senectus molestie ut tincidunt sit convallis amet. Nullam quis urna porttitor nulla ipsum ';
+    }
+  })
+}
