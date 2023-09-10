edf6bd4ec878b58ea604ab70721eeece19e4124a
Mykola Butylkov <80322190+MykolaButylkov@users.noreply.github.com>
2023-09-10 20:48:33 +0300

    6th commit

--- script/script.js
+++ script/script.js
@@ -4,21 +4,52 @@ const footerSlider = document.querySelector('.footer__footer-list-slider');
 const body = document.querySelector('body');
 
 gsap.registerPlugin(ScrollTrigger);
-  gsap.to(footerSlider, {
-    x: () => "-=" +
-      (footerSlider.clientWidth - window.innerWidth),
-    scrollTrigger: {
-      trigger: footerSlider,
-      start: () => `${footerSlider.clientHeight} ${window.innerHeight}`,
-      end: () => `${footerSlider.clientHeight} ${(window.innerHeight - (footerSlider.clientHeight * 2)) }`,
-      scrub: 1,
-      pin: body,
+
+
+gsap.to(footerSlider, {
+  x: () => "-=" +
+    (footerSlider.clientWidth - window.innerWidth),
+  scrollTrigger: {
+    trigger: footerSlider,
+    start: () => `${footerSlider.clientHeight} ${window.innerHeight}`,
+    end: () => `${footerSlider.clientHeight} ${(window.innerHeight - (footerSlider.clientHeight * 2))}`,
+    scrub: 1,
+    pin: body,
+  },
+});
+
+const footerTl = document.querySelector('.footer__footer-slider-timeline');
+gsap.to(footerTl, {
+  scaleX: () => {
+    if (window.innerWidth > 570) {
+      return 4;
+    } else {
+      return 2;
     }
-  });
+  },
+  transformOrigin: 'left',
+  scrollTrigger: {
+    trigger: footerTl,
+    start: 'top bottom',
+    end: `top top`,
+    // start: () => `${footerSlider.clientHeight} ${window.innerHeight}`,
+    // end: () => `${footerSlider.clientHeight} ${(window.innerHeight - (footerSlider.clientHeight * 2))}`,
 
+    markers: true,
+    scrub: 4,
+  }
 
-const footerListDescription = document.querySelectorAll('.footer-list-slider__descr')
+})
+gsap.set('.footer__footer-slider-timeline', {position:'fixed', transformOrigin:'60vh 0'})
 
+// const tl = gsap.timeline();
+gsap.ticker.add(() => gsap.to('.footer__footer-slider-timeline',
+  { duration: 0, x: -gsap.getProperty(footerSlider, 'x') - '110'}))
+
+// window.onload = window.onresize = () => {
+//    gsap.set('.footer__footer-slider-timeline', { left: innerWidth / 2, top: innerHeight / 2 }); }
+
+const footerListDescription = document.querySelectorAll('.footer-list-slider__descr');
 if (window.innerWidth <= 570) {
   footerListDescription.forEach((el, i) => {
     if (i === 0) {
@@ -32,3 +63,11 @@ if (window.innerWidth <= 570) {
     }
   })
 }
+
+gsap.to('.about-us__circl-img', {
+  rotation: 360,
+  scrollTrigger: {
+    trigger: '.about-us__circl-img',
+    scrub: true,
+  }
+})
